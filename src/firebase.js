43a93496208995f57bc7fcebc848d018f6522ad1
septianodwikusuma <septianodwik@gmail.com>
2024-09-16 10:27:10 +0700

    update firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,22 @@
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBMOeI2cMtZPAVFlJH2hOxPMk7KbaWhgDA",
-  authDomain: "chat-app-react-963a2.firebaseapp.com",
-  projectId: "chat-app-react-963a2",
-  storageBucket: "chat-app-react-963a2.appspot.com",
-  messagingSenderId: "747343950890",
-  appId: "1:747343950890:web:b6956f1c0d874249debf53",
-  measurementId: "G-7REF1Z6DWE"
+  apiKey: "AIzaSyCl8dz7_ZymnIBCEXJ87beCwVLf4PQHV0o",
+  authDomain: "chat-app-d80fd.firebaseapp.com",
+  projectId: "chat-app-d80fd",
+  storageBucket: "chat-app-d80fd.appspot.com",
+  messagingSenderId: "536563437458",
+  appId: "1:536563437458:web:5d97799d36e49bf7affb4b",
+  measurementId: "G-FRJZWM021Y"
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 export const auth = getAuth();
 export const storage = getStorage();
 export const db = getFirestore()
